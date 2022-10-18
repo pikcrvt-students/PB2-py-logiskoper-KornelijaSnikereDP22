aa7941bc23d688be1a67f1352265f8770b44d9c2
21DP2KSnik <106649035+KKsnikere@users.noreply.github.com>
2022-10-18 15:22:06 +0300

    Update uzdevums4.py

--- uzdevums4.py
+++ uzdevums4.py
@@ -1,7 +1,7 @@
 from math import sqrt
 point_x=float(input())
 point_y=float(input())
-C=sqrt(point_x**2+point_y**2)
+C=round(sqrt(point_x**2+point_y**2), 2)
 if C<1:
     print("Punkts atrodas figūras iekšā.")
 elif C==1:
