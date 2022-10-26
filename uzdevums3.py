1f64686ef90250ae73d6893cead23707270e537f
21DP2KSnik <106649035+KKsnikere@users.noreply.github.com>
2022-10-26 17:24:51 +0300

    Update uzdevums3.py

--- uzdevums3.py
+++ uzdevums3.py
@@ -1,3 +1,22 @@
+""" 
+Programma pārbauda punktu atrašanās vietu trijstūrī.
+Izveidoja Kasandra Kornelija Snikere DP2-2
+
+(0;0) Punkts atrodas uz figuras robežas.
+(0; 3) Punkts atrodas uz figuras robežas.
+(1.2; 1.2) Punkts atrodas uz figuras robežas.
+(0.1; 2.85) Punkts atrodas uz figuras robežas.
+(1.5; 0.75) Punkts atrodas uz figuras robežas.
+
+(1; 1) Punkts atrodas figūras iekšā.
+(0.4; 1.6) Punkts atrodas figūras iekšā.
+(1.2; 0.2) Punkts atrodas figūras iekšā.
+
+(2; 2) Punkts atrodas ārpus figūras.
+(2; -1) Punkts atrodas ārpus figūras.
+(-1; 1) Punkts atrodas ārpus figūras.
+"""
+
 point_x=float(input())
 point_y=float(input())
 if point_y<0 or point_x<0 or round(point_y*1000)/1000 > round((-1.5*point_x+3)*1000)/1000:
